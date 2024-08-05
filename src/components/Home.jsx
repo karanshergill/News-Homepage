fdc5218e28e0600e7a1dafb3ffb579f20b8889ca
Karan <email@karanshergill.com>
2024-08-05 20:55:07 +0530

    fixed paths

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,9 @@ import { Hero } from './Hero'
 import { New } from './New'
 import { Featured } from './Featured'
 
+import desktopImage from '../assets/image-web-3-desktop.jpg';
+import mobileImage from '../assets/image-web-3-mobile.jpg';
+
 export const Home = () => {
   return (
     <div className='flex flex-col w-full my-8 mx-6 desktop:max-w-5xl md:gap-8 dektop:gap-8 md:my-10 dektop:my-10'>
@@ -10,8 +13,8 @@ export const Home = () => {
       <div className='grid desktop:grid-cols-3 gap-8 mt-6 md:mt-0 desktop:mt-0'>
         <div className='md:col-span-2 desktop:col-span-2'>
           <picture>
-            <source srcSet="../assets/image-web-3-desktop.jpg" media="(min-width: 1440px)" />
-            <img src="../assets/image-web-3-mobile.jpg" alt="Web 3" className="w-full h-auto" />
+            <source srcSet={desktopImage} media="(min-width: 1440px)" />
+            <img src={mobileImage} alt="Web 3" className="w-full h-auto" />
           </picture>
           <Hero />
         </div>
