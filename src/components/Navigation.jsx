2e458954edff0e336894895ad7f26fde3e69fcea
Karan <email@karanshergill.com>
2024-08-05 20:29:20 +0530

    add animation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion";
+
+const navItems = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
 
 export const Navigation = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -12,15 +15,17 @@ export const Navigation = () => {
             <img src="/src/assets/images/logo.svg" className="h-8 w-auto" alt="Logo" />
 
             <button className="block desktop:hidden" onClick={toggleMobileMenu}>
-                {!isMobileMenuOpen ? (
-                    <img src="/src/assets/images/icon-menu.svg" alt="Menu Open" />
-                ) : (
-                    <img src="/src/assets/images/icon-menu-close.svg" alt="Menu Close" />
-                )}
+                <motion.img
+                    src="/src/assets/images/icon-menu.svg"
+                    alt="Menu Open"
+                    initial={{ rotate: 0 }}
+                    animate={{ rotate: isMobileMenuOpen ? 90 : 0 }}
+                    transition={{ duration: 0.3 }}
+                />
             </button>
 
             <ul className='hidden desktop:flex gap-8 text-default text-color-dark-grayish-blue'>
-                {['Home', 'New', 'Popular', 'Trending', 'Categories'].map((item) => (
+                {navItems.map((item) => (
                     <li key={item}>
                         <button className="hover:text-color-soft-orange cursor-pointer focus:outline-none">
                             {item}
@@ -29,29 +34,45 @@ export const Navigation = () => {
                 ))}
             </ul>
 
-            {isMobileMenuOpen && (
-                <div className="fixed top-0 right-0 w-3/4 h-full bg-white z-50">
-                    <div className="flex items-center justify-end p-8">
-                        <button
-                            onClick={toggleMobileMenu}
-                            aria-label="Close Mobile Menu"
-                        >
-                            <img src="/src/assets/images/icon-menu-close.svg" alt="Close Icon" className="w-6 h-6" />
-                        </button>
-                    </div>
-                    <ul className="flex flex-col gap-6 px-6 py-12 text-lg font-medium text-color-very-dark-blue">
-                        {['Home', 'New', 'Popular', 'Trending', 'Categories'].map((item) => (
-                            <li key={item}>
-                                <button
-                                    className="hover:text-color-soft-orange cursor-pointer focus:outline-none"
-                                    onClick={toggleMobileMenu}>
-                                    {item}
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <AnimatePresence>
+
+                {isMobileMenuOpen && (
+                    <motion.div
+                        initial={{ x: '100%' }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 0.5 }}
+                        exit={{ x: '100%' }}
+                        className="fixed top-0 right-0 w-3/4 h-full bg-white z-50">
+
+                        <div className="flex items-center justify-end p-8">
+                            <button
+                                onClick={toggleMobileMenu}
+                                aria-label="Close Mobile Menu"
+                            >
+                                <motion.img
+                                    src="/src/assets/images/icon-menu-close.svg"
+                                    alt="Close Icon"
+                                    className="w-6 h-6"
+                                    initial={{ rotate: 0 }}
+                                    animate={{ rotate: isMobileMenuOpen ? 90 : 0 }}
+                                    transition={{ delay: 0.3, duration: 0.3 }}
+                                />
+                            </button>
+                        </div>
+                        <ul className="flex flex-col gap-6 px-6 py-12 text-lg font-medium text-color-very-dark-blue">
+                            {navItems.map((item) => (
+                                <li key={item}>
+                                    <button
+                                        className="hover:text-color-soft-orange cursor-pointer focus:outline-none"
+                                        onClick={toggleMobileMenu}>
+                                        {item}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </nav>
     )
 }
